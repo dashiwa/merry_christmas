6ce2af66091710674411ae128ed2cd821427f211
dashiwa <moscovie.com@gmail.com>
2016-01-03 10:45:40 +0300

    fix sound bug

--- merry_christmas/js/merry_christmas.js
+++ merry_christmas/js/merry_christmas.js
@@ -11,7 +11,7 @@
             });
 
             function f() {
-                var i = "/sites/default/files/ny.swf";
+                var i = "ny.swf";
                 i = i + "?nc=" + (new Date().getTime());
                 swfobject.embedSWF(i, "z-audio__player", "1", "1", "9.0.0", null, {}, {
                     allowScriptAccess: "always",
